2926bf0d16f368a3790377bb2717ffbc576f48a1
Badou <bryan.fiarda@outlook.com>
2022-04-02 21:35:19 +0200

    Learning Python

---
+++ main.py
@@ -0,0 +1,14 @@
+# Lean Python Go Work ! #
+
+# Price + Apple + #
+
+prices = {'apple': 0.75, 'egg': 0.50}
+cart = {
+    'apple': 1,
+    'egg': 6
+}
+
+bill = sum(prices[item] * cart[item]
+           for item in cart)
+
+print(f'I have to pay {bill:.2f}')
